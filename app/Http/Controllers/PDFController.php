5639552095b33ef9bad2feaa80004c872f4e6b41
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2019-08-05 23:28:28 +0900

    [update] pdfを表示するように修正

--- app/Http/Controllers/PDFController.php
+++ app/Http/Controllers/PDFController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PDF;
 
 class PDFController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $pdf = PDF::loadHTML('<h1>Hello World</h1>');
+
+        return $pdf->stream();
+
+    }
 }
